d5a309da6df91d99a091618bc3795ac48fc92a31
joaopedropastura <jpsoares111@hotmail.com>
2023-10-19 11:53:44 -0300

    file read done

--- main.py
+++ main.py
@@ -2,60 +2,116 @@ import random
 
 
 
-values = [[0,1,2] , [3,4,5] , [6,7,8] , [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+plays = [[0,1,2] , [3,4,5] , [6,7,8] , [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 totalgames = []
-
-
+aval = 0
 
 def createTables(qtt):
     table = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0], False]
     for i in range(qtt):
         totalgames.append(table)
         
+        
+
+
+def minimax(ismax, table):
+    result = Eval(ismax);
+    
+    if(result != 0):
+        return result
+    
+    if(ismax):
+        biggestScore = -1000
+        for i in range(len(totalgames)):
+            if not totalgames[i][2]:
+                
+                for position in totalgames[i][1]:
+                    if position == 0:
+                        position = 1
+                        totalgames[i][2] = verifyTableEnd(i)
+                        score = minimax(True, totalgames[i][1], False)
+                        position = 0
+                        biggestScore = max(score, biggestScore)        
+        return biggestScore
+    
+    else:
+        biggestScore = 1000
+        for i in range(len(totalgames)):
+            if totalgames[i][2]:
+                
+                for position in totalgames[i][1]:
+                    if position == 0:
+                        position = 1
+                        totalgames[i][2] = verifyTableEnd(i)
+                        score = minimax(True, totalgames[i][1], True)
+                        position = 0
+                        biggestScore = min(score, biggestScore)
+                        
+        return biggestScore
+        
+                        
+        
+    
+    
+    
+def Eval(myturn):
+    for table in totalgames:
+        for isend in table[2]:
+            if(myturn and isend):
+                return -1
+            elif(not myturn and isend):
+                return 1
+            else:
+                return 0
+        
+    
+    
+def verifyTableEnd(game):
+    resultGame = totalgames[game][0]
+    for i in range(len(resultGame)):
+        if(resultGame[i] >= 3):
+            return True
+    return False
+    
     
 def gameTurn(position,game):
     tableGame = totalgames[game][1]
     resultGame = totalgames[game][0]
-    win = totalgames[game][2]
-    
-    if(win):
-        return win
-    
     
     if(tableGame[position] == 1):
         print("posição ja selecionada")
     else:
         tableGame[position] += 1
         
-    
-    for i in range(len(values)-1):
-        if(position in values[i]):
-            resultGame[i] += 1
-    
-    
-    for i in range(len(resultGame)):
-        if(resultGame[i] >= 3):
-            return True
         
-    return False
-                
+    for i in range(len(plays)-1):
+        if(position in plays[i]):
+            resultGame[i] += 1
+
+    
+
+
+
 
 
 createTables(3)
 game = 2
-
 while True:
-    while True:
-        computer = random.randint(0, 8)
-        print(computer)
-        if(totalgames[game][1][computer] == 0):
-            break
-        
-    totalgames[2][2] = gameTurn(computer,game)
+    
+    
+    gameTurn(computer, game)
+    totalgames[2][2] = verifyTableEnd(game)
+    
+
     print(totalgames[game][1][0], totalgames[game][1][1], totalgames[game][1][2])
     print(totalgames[game][1][3], totalgames[game][1][4], totalgames[game][1][5])
     print(totalgames[game][1][6], totalgames[game][1][7], totalgames[game][1][8])
     
     posicao = int(input("escolha sua posicao: "))
-    totalgames[2][2] = gameTurn(posicao,game)
+    gameTurn(posicao, game)
+    totalgames[2][2] = verifyTableEnd(game)
+    
+    print(totalgames[game][1][0], totalgames[game][1][1], totalgames[game][1][2])
+    print(totalgames[game][1][3], totalgames[game][1][4], totalgames[game][1][5])
+    print(totalgames[game][1][6], totalgames[game][1][7], totalgames[game][1][8])
     
--- mainM1.py
+++ mainM1.py
@@ -1,52 +1,52 @@
 import sys
 import os
-    
+import random
+
+random.seed(8)
+
 player = sys.argv[1]
 tables = sys.argv[2]
 
 files = ['m1.txt', 'm2.txt']
 
-old_name = r"C:/Users/disrct/Desktop/AIcsharpJp/notakto-ai/front/m2 last.txt"
-new_name = r"C:/Users/disrct/Desktop/AIcsharpJp/notakto-ai/front/m1.txt"
-
 
 def UpdateFileValue(file_name):
-    f = open(file_name, "r")
-    print(f.read())
-
+    f = open(file_name, 'w')
+    f.write('0 ' + str(round(random.random() * 1)))
+    f.close()
 
 def GetFileValue():
     print('')
 
-
-
 def GetFileName():
+    file_name = ''
     for f_name in os.listdir('front'):
-        if f_name.endswith('last.txt'):
-            return f'./front/{f_name}'
-        else:
-            return open("./front/m1.txt", "a").write('0 1')
-        
+        if f_name.endswith('.txt'):
+            file_name = f'./front/{f_name}'
+    
+    if(file_name == '' and player == 'm1'):
+        f = open("./front/m1.txt", "w")
+        f.write('0 ' + str(round(random.random() * 10)))
+        f.close()
+        file_name = "./front/m1.txt"
+    return file_name
 
 def FileManipulation():
     file_name = GetFileName()
-    if(player == 'm1'):
-        os.rename(file_name,'./front/m1.txt')
-    else:
-        os.rename(file_name,'./front/m2.txt')
-    UpdateFileValue(file_name)
-        # for f_name in os.listdir('./front'):
-        #     file = f_name.endswith('.txt')
-        # os.rename(f'./front/{file}.txt','./front/m1.txt')
+    if ('m1 last' in file_name):
+        if (player == 'm1'):
+            print(file_name)
+            os.remove(file_name)
+            file_name = "./front/m1.txt"
+            UpdateFileValue(file_name)
 
-        # f = open("./front/m1.txt", "a").truncate(0)
-        # f.write("Now the file has more content!")
-        # f.close()
-    # else:
-    #     os.rename('./front/m1 last.txt','./front/m2.txt')
-    #     f = open("./front/m1.txt", "a")
-    #     f.write("Now the file has more content!")
-    #     f.close()
+    elif ('m2 last' in file_name):
+        if(player == 'm2'):
+            os.remove(file_name)
+            file_name = "./front/m2.txt"
+            UpdateFileValue(file_name)
+            
 
 
-FileManipulation()
+while(True):
+    FileManipulation()
